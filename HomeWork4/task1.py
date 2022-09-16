599a0c1a2858207ef1567d2d3dafd6c91cda8c4d
IrinaGibadullina <gibadullina.irina@me.com>
2022-09-16 10:24:57 +0500

    Вычислить число c заданной точностью d

--- HomeWork4/task1.py
+++ HomeWork4/task1.py
@@ -2,3 +2,13 @@
 #Пример:
 #- при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
 
+num = 7.3245678 
+  
+# using "%" operator 
+print('The value is: %.2f'%num)  
+  
+# using format() function 
+print("The value is: {0:.5f}".format(num))  
+  
+# using round() function 
+print("The value is:",round(num,1)) 
